af852bd070cf3af3df272ea20adbc868a129d448
Леонид <lenya.kozlovskiy.97@mail.ru>
2025-01-20 15:44:05 +0300

    Project Configuration Setup (#1)
    * feat:install vite react and ts
    
    * feat:add elsint and add configuration rules yo it
    
    * feat:add prettier and base configuration on it

---
+++ airLines/eslint.config.js
@@ -0,0 +1,23 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/explicit-function-return-type": "error",
+      "no-unused-vars": "off",
+      "no-undef": "off",
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+    },
+  },
+];
