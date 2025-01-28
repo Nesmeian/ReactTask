bb44de438a5a329c2eaa654fa3b29eac04993c6c
Nesmeian <lenya.kozlovskiy.97@mail.ru>
2025-01-28 13:01:50 +0300

    feat:locate CircularIndeterminate in center of the page

--- ReactPokemonsApi/src/utils/CircularIndeterminate.tsx
+++ ReactPokemonsApi/src/utils/CircularIndeterminate.tsx
@@ -4,7 +4,14 @@ import Box from '@mui/material/Box'
 export default function CircularIndeterminate(): JSX.Element {
     return (
         <Box sx={{ display: 'flex' }}>
-            <CircularProgress size={120} color={'primary'} />
+            <CircularProgress
+                size={120}
+                sx={{
+                    position: 'absolute',
+                    top: '45%',
+                    left: '50%',
+                }}
+            />
         </Box>
     )
 }
